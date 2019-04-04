4587cb5c4f2c5af9afeb01ffb647e7bc77dab9b6
lowaiis10 <49284254+lowaiis10@users.noreply.github.com>
2019-04-05 01:03:14 +0800

    Update index.py

--- index.py
+++ index.py
@@ -4,11 +4,11 @@ if __name__ == '__main__':
 	print('starting...')
 	start = time.time()
 	min_yield = 8
-  url = 'www.some_website_with_data.com'
-  
-  create_url_list(url)
-  get_company_fundmentals()
-  clean_and_process_data(min_yield)
+	url = 'www.some_website_with_data.com'
+
+	create_url_list(url)
+	get_company_fundmentals()
+	clean_and_process_data(min_yield)
   
 	end = time.time()
 	print('End// /ntime taken: ', end-start)
